c3210979de694bba876b3e025d64424e534fb476
jatink-22 <168222260+jatink-22@users.noreply.github.com>
2025-02-17 06:14:31 +0000

    Demo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PostgresConnectivityApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
